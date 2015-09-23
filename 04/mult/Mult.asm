8c3a9dbee7199d1f7fdf497632657491bd0d75bc
Hiraku NAKANO <hiraku@tojiru.net>
2015-09-23 14:09:21 +0900

    implements Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,22 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R2
+    M=0   // R2 <- 0
+
+(LOOP)
+    @R1
+    D=M      // D <- R1
+    @END
+    D;JLE    // if D(=R1) <= 0 then goto END
+    @R0
+    D=M      // D <- R0
+    @R2
+    M=D+M    // R2 += R0
+    @R1
+    M=M-1    // R1--
+    @LOOP
+    0;JMP // LOOPへ戻る
+(END)
+    @END
+    0;JMP
