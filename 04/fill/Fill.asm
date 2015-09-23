f8cb77b947d1b13bc9bae509492c9c115f3b26df
Hiraku NAKANO <hiraku@tojiru.net>
2015-09-23 15:51:06 +0900

    implements Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,50 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(INIT)
+    @SCREEN
+    D=A      // D <- SCREENのアドレス
+    @addr    // 塗りつぶしたいSCREEN上のワードを表す
+    M=D      // 初期値はSCREENアドレス
+
+    // キーボードの状態を見て、塗りつぶす色を選択
+    @KBD
+    D=M
+    @IFPRESS
+    D;JGT
+    @ELSE
+    0;JMP
+(IFPRESS)
+    @color
+    M=-1     // color=-1 黒
+    @ENDIF
+    0;JMP
+(ELSE)
+    @color
+    M=0      // color=0 白
+(ENDIF)
+
+(LOOP)       //塗りつぶすループ
+    @color
+    D=M      // D <- color
+    @addr
+    A=M
+    M=D      // *addr <- color
+    @addr
+    M=M+1
+
+    @8192
+    D=A
+    @SCREEN
+    D=D+A
+    @addr
+    D=D-M  // D= 8192 + SCREEN - addr
+    // 塗り残しがあれば塗りつぶすまで続ける
+    @LOOP
+    D;JGT
+
+    // 全部塗り終わったら、最初に戻る
+    @INIT
+    0;JMP
+
+// このプログラムは永久に終了しない
